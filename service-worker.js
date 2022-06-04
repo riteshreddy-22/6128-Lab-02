eaa0154a64bc45cd3c8c7ffd6aa03198e0f71afe
riteshreddy-22 <riteshreddy1682@gmail.com>
2022-06-04 10:10:06 -0400

    Changes

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
-self.addEventListener('fetch', () => {
+// Installing a service worker
+self.addEventListener('install', (event) => {
+    console.log('SW installed: ', event);
+    self.skipWaiting();
+});
+
+// Activating a service worker
+self.addEventListener('activate', (event) => {
+    console.log('SW Activated: ', event);
+    event.WaitUntil(clients.claim());
+});
+
+// Fetch event from service worker
+self.addEventListener('fetch', (event) => {
     return;
-    });
+ });
